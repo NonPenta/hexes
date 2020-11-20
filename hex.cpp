f076d1c514317d1ae0dca0d9af13d50333d6d25d
= <victor.j.l.bernard@gmail.com>
2020-11-20 14:56:09 +0100

    also just added a bit of space there

--- hex.cpp
+++ hex.cpp
@@ -3,22 +3,29 @@
 
 Hex::Hex() {}
 Hex::Hex(int x_, int y_): p(x_, y_), size(10), s(size-1, 6) {
+
   s.setOutlineThickness(1);
+
   if ((x_%65==0) || (y_%65==0)) {
     s.setFillColor(sf::Color(255, 0, 0));
   } else {
     s.setFillColor(sf::Color(32, 31, 35));
   }
+
   s.setOutlineColor(sf::Color(48,47,51));
-  s.setPosition(size*std::sqrt(3.0)*x_ + size*std::sqrt(3.0)/2*y_, size*3*y_/2);
+  s.setPosition(size*std::sqrt(3.0)*x_ + size*std::sqrt(3.0)/2.*y_, size*3.*y_/2.);
 }
+
 Hex::Hex(int x_, int y_, float size_): p(x_, y_), size(size_), s(size-1, 6) {
+
   s.setOutlineThickness(1);
+
   if ((x_%65==0) || (y_%65==0)) {
     s.setFillColor(sf::Color(255, 0, 0));
   } else {
     s.setFillColor(sf::Color(32, 31, 35));
   }
+
   s.setOutlineColor(sf::Color(48,47,51));
   s.setPosition(size*std::sqrt(3.0)*x_ + size*std::sqrt(3.0)/2*y_, size*3*y_/2);
 }
