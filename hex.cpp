c1f79dc6977fa78ad3d3e4b4656b1a0752cfe6b0
= <victor.j.l.bernard@gmail.com>
2020-11-26 10:55:39 +0100

    Corrected the offset (i think i put the origin in the center of a hex)

--- hex.cpp
+++ hex.cpp
@@ -14,6 +14,7 @@ Hex::Hex(int x_, int y_): p(x_, y_), size(10), s(size-1, 6) {
 
   s.setOutlineColor(sf::Color(48,47,51));
   s.setPosition(size*std::sqrt(3.0)*x_ + size*std::sqrt(3.0)/2.*y_, size*3.*y_/2.);
+  s.setOrigin(9.7, 12);
 }
 
 Hex::Hex(int x_, int y_, float size_): p(x_, y_), size(size_), s(size-1, 6) {
