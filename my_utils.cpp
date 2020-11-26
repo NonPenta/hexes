001c3150724f6897185a72b0ad9b8482a93458fe
= <victor.j.l.bernard@gmail.com>
2020-11-26 10:55:04 +0100

    Added a few utils

---
+++ my_utils.cpp
@@ -0,0 +1,44 @@
+#include <SFML/Graphics.hpp>
+
+int abs(int x) { if (x > 0) { return x; } else { return -x; }}
+int abs(float x) { if (x > 0) { return x; } else { return -x; }}
+
+sf::Vector2i hex_round(sf::Vector2f f_pos) {
+  int x = std::round(f_pos.x);
+  int y = std::round(f_pos.y);
+  int z = - x - y;
+
+  float dx = abs(x - f_pos.x);
+  float dy = abs(y - f_pos.y);
+  float dz = abs(z + f_pos.x + f_pos.y);
+
+  if (dx>dy && dx>dz) {
+    x = - y - z;
+  } else if (dy>dz) {
+    y = - x - z;
+  }
+
+  return sf::Vector2i(x, y);
+}
+
+sf::Vector2i hex_from_pix(sf::Vector2f pos) {
+  float hex_x = (std::sqrt(3.)/3 * pos.x - 1/3. * pos.y)/10. ;
+  float hex_y = 2/3. * pos.y/10.;
+
+  return hex_round(sf::Vector2f(hex_x, hex_y));
+}
+
+sf::Vector2i chunk_from_hex(sf::Vector2i hex_pos) {
+  int x = (hex_pos.x+32)/65 - (hex_pos.x+32 < 0);
+  int y = (hex_pos.y+32)/65 - (hex_pos.y+32 < 0);                               // cpp's division is fucking bad. it's trash.
+
+  return sf::Vector2i(x, y);
+}
+
+sf::Vector2i hex_within_chunk(sf::Vector2i hex_pos) {
+  sf::Vector2i chunk_pos = chunk_from_hex(hex_pos);
+  int x = hex_pos.x - chunk_pos.x*65+32;
+  int y = hex_pos.y - chunk_pos.y*65+32;
+
+  return sf::Vector2i(x, y);
+}
