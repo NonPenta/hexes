702e53ea682b9acc624b45e06b3cf63770819849
= <victor.j.l.bernard@gmail.com>
2021-01-18 16:23:12 +0100

    changed some useless color change in default constructor

--- hex.cpp
+++ hex.cpp
@@ -3,15 +3,8 @@
 
 Hex::Hex() {}
 Hex::Hex(int x_, int y_): type{"n"}, p{x_, y_}, size{10}, s{size-1, 6} {
-
   s.setOutlineThickness(1);
-
-  if ((x_%65==0) || (y_%65==0)) {
-    s.setFillColor(sf::Color(255, 0, 0));
-  } else {
-    s.setFillColor(sf::Color(32, 31, 35));
-  }
-
+  s.setFillColor(sf::Color(32, 31, 35));
   s.setOutlineColor(sf::Color(48,47,51));
   s.setPosition(size*std::sqrt(3.0)*x_ + size*std::sqrt(3.0)/2.*y_, size*3.*y_/2.);
   s.setOrigin(9.7, 12);
