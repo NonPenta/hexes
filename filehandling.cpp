5583ff1296cc4ce2cbcab01bd9d31994e0c3ed2d
= <victor.j.l.bernard@gmail.com>
2021-01-18 16:28:12 +0100

    "properly" commented filehandling

--- filehandling.cpp
+++ filehandling.cpp
@@ -13,7 +13,7 @@ void save(ChunkMap cmap, std::string savename) {
 
   for (const auto &chunk_pair : cmap.getMap()) {
     std::string lastType = "no.";
-    std::cout << "one down, some more to go ?" << '\n';
+
     // create file with name : "chunk_x:chunk_y"
     std::fstream chunkfile;
     chunkfile.open(savepath/(std::to_string(chunk_pair.first.first)+":"+std::to_string(chunk_pair.first.second)), std::fstream::app);
@@ -52,61 +52,51 @@ ChunkMap load(std::string savename) {
     std::vector<int> chunkPos;
     std::string tok;
 
-    std::cout << "created chunk position vector and token" << '\n';
-
-    for (int i=0;i<filenameStr.length();i++) {
-      std::cout << "index in filename : " << i << '\n';
+      // --- PARSING FROM FILENAME ----
+    for (int i = 0; i < (int)filenameStr.length(); i++) {
       char ch = filenameStr[i];
       if (ch == ';') {
         chunkPos.push_back(std::stoi(tok));
         tok = "";
       } else {tok += ch;}
     }
-      // converting the vector to a pair
+
+      // --- CONVERTING FROM VECTOR TO PAIR
     std::pair<int,int> chunk_pos;
     chunk_pos.first = chunkPos[0];
     chunk_pos.second = chunkPos[1];
 
-    std::cout << "chunk pos done" << '\n';
-
-    // putting the shit from the file in the chunkmap etc
-    Chunk c = Chunk{chunk_pos.first, chunk_pos.second}; // in theory thats correct but not sure im stoopid
-
-    std::cout << "chunk created with chunk pos" << '\n';
-
+    // --- CREATING CHUNK ---
+    Chunk c = Chunk{chunk_pos.first, chunk_pos.second};
+    // --- CREATING FILE STREAM AND LINE
     std::fstream chunkfile(cfile.path());
     std::string line;
 
-    std::cout << "fstream and line done" << '\n';
-
+    // --- TREATING FILE ---
     while (std::getline(chunkfile, line)) {
 
-      std::cout << "treating line" << '\n';
-
+      // --- PARSING SETUP ---
       std::string prevTok;
       std::string token;
-
       int n = 0;
-      for (int i = 0; i<line.length(); i++) {
-        std::cout << "parsing line. i = " << i << '\n';
 
+      // ---- PARSING ----
+      for (int i = 0; i < (int)line.length(); i++) {
         char ch=line[i];
-        std::cout << "char converted at i = " << i << '\n';
-        if (ch == ';') {
-          std::cout << "separation between values, val " << n << '\n';
+        if (ch == ';') { // SEPARATOR
+
           c.setHexType(n%65-32,n/65-32, token);
-          std::cout << "hex position : " << n/65 << " " << n%65 << '\n';
+
           prevTok = token;
           token = "";
           n++;
 
-        } else if (ch == '/') {
+        } else if (ch == '/') { // SAME CONTENT
             token = prevTok;
-
-        } else {token += ch;}
+        } else {token += ch;} // ADD TO CONTENT
       }
     }
-
+    // --- PUT CHUNK CONTENT IN MAP ---
     map.setChunk(chunk_pos, c);
 
   }
