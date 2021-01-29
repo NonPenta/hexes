a04cc21147e25985e29149698d9855008b920622
= <victor.j.l.bernard@gmail.com>
2021-01-29 10:40:05 +0100

    "It's a secret tool that xill help us later"

--- context.hpp
+++ context.hpp
@@ -1,6 +1,11 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include "chunkmap.hpp"
+#include "entity.hpp"
+
 struct Context {
   ChunkMap &map;
   std::unordered_map<std::string, Entity> &entities;
   sf::RenderWindow &window;
+  sf::View view;
 };
